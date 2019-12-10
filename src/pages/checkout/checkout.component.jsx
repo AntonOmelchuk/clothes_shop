d1f60cd596113b4ba5a9ff8d6ba15f22bd7d89c9
Anton Omelchuk <kino_kg@ukr.net>
2019-12-10 09:31:16 +0200

    correct misspell

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,7 +36,7 @@ const CheckoutPage = ({cartItems, total}) => (
     ))}
     <div className='total'>Total ${total}</div>
     <div className='test-card'>
-      Test credit cart for payment
+      Test credit card for payment
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CCV: 123
     </div>
