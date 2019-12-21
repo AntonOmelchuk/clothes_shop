6ea12d7da647cb92ddbd298919edc27219534456
Anton Omelchuk <kino_kg@ukr.net>
2019-12-21 16:46:28 +0200

    add express server for stripe payment

--- client/src/components/stripe/stripe-checkout.component.jsx
+++ client/src/components/stripe/stripe-checkout.component.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 export const StripeCheckoutButton = ({price}) => {
   const stripePrice = price * 100;
   const stripeKey = 'pk_test_rnavDcr0xgjPmloqM2xAJW1300a1oKVYCy';
 
-  const onToken = () => alert('Payment successful');
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: stripePrice,
+        token,
+      },
+    })
+      .then(res => alert('Payment successful'))
+      .catch(err => {
+        alert('There was an issue with your payment');
+        console.log('Payment error ' + err);
+      });
+  };
 
   return (
     <StripeCheckout
